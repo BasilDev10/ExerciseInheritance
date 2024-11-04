1d130040631fdb23370c3ea10a85426b475fbb1f
Basil <basilaltasan@gmail.com>
2024-11-04 14:53:46 +0300

    upload 3

--- src/Main.java
+++ src/Main.java
@@ -8,16 +8,17 @@ public class Main {
         Shape shape = new Shape("Black",true);
         Shape shape2 = new Shape("Red",false);
         Shape shape3 = new Shape();
+
         System.out.println("---------------Test Shape--------------");
         System.out.println(shape);
         System.out.println(shape2);
         System.out.println(shape3);
         System.out.println("---------------End Test Shape--------------");
+
         Circle circle = new Circle();
-
         Circle circle2 = new Circle(5);
-
         Circle circle3 = new Circle(7,"Grey",true);
+
         System.out.println("---------------Test Circle--------------");
         System.out.println(circle);
         System.out.println(circle.getArea());
@@ -31,6 +32,7 @@ public class Main {
         System.out.println(circle3.getArea());
         System.out.println(circle3.getPerimeter());
         System.out.println("---------------End Test Circle--------------");
+
         Rectangle rectangle = new Rectangle();
         Rectangle rectangle2 = new Rectangle(250,200);
         Rectangle rectangle3 = new Rectangle(100,50,"Red",true);
@@ -53,6 +55,7 @@ public class Main {
         Square square2 = new Square(5);
         Square square3 = new Square(7,"Grey",true);
         Square square4 = new Square();
+        
         System.out.println("---------------Test Square--------------");
         System.out.println(square);
         System.out.println(square.getSide());
